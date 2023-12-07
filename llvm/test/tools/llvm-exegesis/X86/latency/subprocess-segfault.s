378db5b644b4bf4282739b5a49f9b4465802dce7
Aiden Grossman <agrossman154@yahoo.com>
2023-12-07 01:12:20 -0800

    Change segfault address in test

--- llvm/test/tools/llvm-exegesis/X86/latency/subprocess-segfault.s
+++ llvm/test/tools/llvm-exegesis/X86/latency/subprocess-segfault.s
@@ -2,7 +2,7 @@
 
 # RUN: llvm-exegesis -mtriple=x86_64-unknown-unknown -mode=latency -snippets-file=%s -execution-mode=subprocess | FileCheck %s
 
-# CHECK: error:           The snippet crashed with signal Segmentation fault at address 10000
+# CHECK: error:           The snippet crashed with signal Segmentation fault at address 10
 
-# LLVM-EXEGESIS-DEFREG RBX 10000
+# LLVM-EXEGESIS-DEFREG RBX 10
 movq (%rbx), %rax
